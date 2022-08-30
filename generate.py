f8546913be93dc00c9a48192c25eb00372c2626d
petosbratok <44753312+petosbratok@users.noreply.github.com>
2022-08-30 14:32:36 +0300

    'TEst'

---
+++ generate.py
@@ -0,0 +1,4 @@
+import time
+import os
+
+os.system('node num_sender.js')
---
+++ num_sender.js
@@ -0,0 +1,17 @@
+const WebSocket = require('ws');
+
+let url = `ws://testtask00.herokuapp.com/ws/socket-server/`
+
+const chatSocket = new WebSocket(url)
+function send_message(){
+  chatSocket.send(JSON.stringify({
+    'message': Math.floor(Math.random() * (999 - 100 + 1) + 100) + '-' + Math.floor(Math.random() * (999 - 100 + 1) + 100)
+  }))
+}
+async function funq(){
+  while (true){
+    await new Promise(r => setTimeout(r, 5000));
+    await send_message()
+  }
+}
+funq()
