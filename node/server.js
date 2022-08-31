758145615a20dad979a3e533d4f52d1816f9d465
petosbratok <44753312+petosbratok@users.noreply.github.com>
2022-08-31 16:41:06 +0300

    CORS implemented

--- node/server.js
+++ node/server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
+
+const cors = require('cors');
+app.use(cors({
+    origin: '*',
+}));
+
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+
+const io = new Server(server, {
+  cors: {
+    origin:'*',
+  }
+});
+
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
