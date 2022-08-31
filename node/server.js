f6c0672ddce2f1a51b37f624a35caef9b1b62a73
petosbratok <44753312+petosbratok@users.noreply.github.com>
2022-08-31 14:33:31 +0300

    The task is ready

---
+++ node/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', (socket) => {
+  socket.on('new_data', (data) => {
+    io.emit('new_data', data);
+  });
+  console.log('a user connected');
+});
+
+
+
+server.listen(process.env.PORT || 5000, function() {
+  console.log('listening on ??? some port')
+})
